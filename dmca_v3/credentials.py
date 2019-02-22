f2a6ad4ddad2844a3493c057a84989e1ae0a8c50
Jonathan Kung <jkung2314@gmail.com>
2019-02-21 19:51:22 -0800

    Add credentials.py

---
+++ dmca_v3/credentials.py
@@ -0,0 +1,17 @@
+__author__ = 'ajanakos, maria, brian, jhkung'
+
+dmcauser = ''
+dmcapass = '' # base64 encoded
+
+es_server = ''
+es_port = 
+es_username = ''
+es_password = ''
+ca_certs = ''
+
+LDAP_USERNAME = ''
+LDAP_PASSWORD = ''
+UCSC_LDAP_SERVER = ''
+UCSC_LDAP_DN = ''
+UCSC_LDAP_FIELDS = ['']
+UCSC_LDAP_BIND_DN = ""
